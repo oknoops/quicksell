98cebd3aac82cced4a31115274db37ff5c60ae0c
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-12 13:12:20 +0100

    adde validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :sale
   belongs_to :user
+  validates_numericality_of :rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, :message => "Rating needs to be betwen 0 and 5"
 end
