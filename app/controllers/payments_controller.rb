4747ad85bdadb9cf832ee773ba6d44b4f51abbdc
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-13 17:05:09 +0100

    done for the day

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,5 @@
 class PaymentsController < ApplicationController
 
-
   def new
     @amount = Sale.new
     authorize @amount
@@ -12,37 +11,20 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    @amount = Sale.new
+    authorize @amount
     @amount = params[:amount]
-
-    @amount = @amount.gsub('€', '').gsub(',', '')
-
-    begin
-      @amount = Float(@amount).round(2)
-    rescue
-      flash[:error] = 'payment not completed. Please enter a valid amount in USD ($).'
-      redirect_to new_payment_path
-      return
-    end
-
-    @amount = (@amount * 100).to_i # Must be an integer!
-
-    if @amount < 500
-      flash[:error] = 'payment not completed. Donation amount must be at least $5.'
-      redirect_to new_payment_path
-      return
-    end
-
     Stripe::Charge.create(
       source: params[:stripeToken],
       amount: @amount,
-      currency: 'usd',
+      currency: 'eur',
       description: 'Custom donation'
     )
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to new_payment_path
-      current_user.wallet += @amount
-      current_user.save
+    current_user.wallet += @amount
+    current_user.save
+    redirect_to new_payment_path
   end
 end
