2fdda69ed39d3638deeda47ebaee6af2d401617b
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-15 17:21:36 +0100

    done for today

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   validates_presence_of :name
   validates_presence_of :price
+  validates_numericality_of :price, greater_than_or_equal_to: 0
   # validates_presence_of :picture
   validates_presence_of :pickup_time
   validates_presence_of :pickup_address
