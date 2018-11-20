8fd54ddb747e6f9de2bad8547725f9dc6baf0c01
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-20 16:12:46 +0100

    test chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,14 @@
 class ChatRoomsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:id])
+    @chat_room = ChatRoom.new(name: @product)
+    authorize @chat_room
+    @chat_room.save
+    redirect_to chat_room_path(@chat_room)
+  end
+
+
   def show
     @chat_room = ChatRoom.includes(messages: :user).find(params[:id])
     authorize @chat_room
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@chat_room = ChatRoom.create(name: "new Chat")
